8dfc36881c82b19433a2aa448ea7aabbcb76a0e3
wallacewangchao <wallacewangchao@gmail.com>
2021-11-22 00:02:48 +0100

    finish the basic configeration of home environment: 1. adjusted the shadow.bias, camera left right and fov to make a right shadow 2. change the funitures scale 2

--- js/main.js
+++ js/main.js
@@ -10,15 +10,23 @@ function init(){
   scene = new THREE.Scene();
   scene.background = new THREE.Color( "rgb(255, 255, 255)" );
   
-  camera = new THREE.PerspectiveCamera( 40, THREEJS_CONTAINER.clientWidth / THREEJS_CONTAINER.clientWidth, 0.1, 1000 );
+  camera = new THREE.PerspectiveCamera( 25, THREEJS_CONTAINER.clientWidth / THREEJS_CONTAINER.clientHeight, 0.1, 1000 );
+  // camera = new THREE.OrthographicCamera( THREEJS_CONTAINER.clientWidth / - 2, THREEJS_CONTAINER.clientWidth / 2, THREEJS_CONTAINER.clientHeight / 2, THREEJS_CONTAINER.clientHeight / - 2, 1, 1000 );
 
   renderer = new THREE.WebGLRenderer();
-  renderer.setSize( THREEJS_CONTAINER.clientWidth, THREEJS_CONTAINER.clientWidth );
+  renderer.setSize( THREEJS_CONTAINER.clientWidth, THREEJS_CONTAINER.clientHeight );
   
   renderer.outputEncoding = THREE.sRGBEncoding;
-
   renderer.shadowMap.enabled = true;
   renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+  // renderer.shadowCameraNear = 1;
+  // renderer.shadowCameraFar = 500;
+  // renderer.shadowCameraFov = 60;
+
+  // renderer.shadowMapBias = -0.0005;
+  // renderer.shadowMapDarkness = 1;
+  // renderer.shadowMapWidth = 2048;
+  // renderer.shadowMapHeight = 2048;
 
   THREEJS_CONTAINER.appendChild( renderer.domElement );
   
@@ -31,94 +39,97 @@ function init(){
   // const directionalLight = new THREE.DirectionalLight( "rgb(255, 255, 255)", 0.8 );
   // scene.add( directionalLight );
   
-  // LIGHTS
-  // const hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.4 );
-  // hemiLight.color.setHSL( 0.6, 1, 0.6 );
-  // hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
-  // hemiLight.position.set( 0, 50, 0 );
-  // scene.add( hemiLight );
+  /**
+   * LIGHTS
+  **/
+  const hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.5 );
+  hemiLight.color.setHSL( 0.6, 1, 0.6 );
+  hemiLight.position.set( 300, 500, 200 );
+  // hemiLight.castShadow = true;
 
-  // const hemiLightHelper = new THREE.HemisphereLightHelper( hemiLight, 10 );
-  // scene.add( hemiLightHelper );
+  scene.add( hemiLight );
 
-  // // direction light
-  // const dirLight = new THREE.DirectionalLight( "rgb(255, 255, 255)", 1 );
-  // dirLight.color.setHSL( 0.1, 1, 0.95 );
-  // dirLight.position.set( - 1, 1.75, 1 );
-  // dirLight.position.multiplyScalar( 10 );
-  // scene.add( dirLight );
+  const hemiLightHelper = new THREE.HemisphereLightHelper( hemiLight, 10 );
+  scene.add( hemiLightHelper );
 
-  // dirLight.castShadow = true;
-  // const dirLightHelper = new THREE.DirectionalLightHelper( dirLight, 10 );
-  // scene.add( dirLightHelper );
+  // direction light
+  const dirLight = new THREE.DirectionalLight( "rgb(255, 255, 255)", 1 );
+  dirLight.color.setHSL( 0.1, 1, 0.95 );
+  dirLight.position.set( - 1, 1.75, 1 );
+  dirLight.position.multiplyScalar( 10 );
 
-  const light1 = new THREE.DirectionalLight( 0xefefff, 1.5 );
-  light1.position.set( 1, 1, 1 ).normalize();
-  scene.add( light1 );
-  light1.castShadow = true;
+  const d = 8;
 
-  const light1Helper = new THREE.DirectionalLightHelper( light1, 10 );
-  scene.add( light1Helper );
+  dirLight.shadow.camera.left = - d;
+  dirLight.shadow.camera.right = d;
+  dirLight.shadow.camera.top = d;
+  dirLight.shadow.camera.bottom = - d;
 
-  const light2 = new THREE.DirectionalLight( 0xffefef, 1 );
-  light2.position.set( - 1, - 1, - 1 ).normalize();
-  scene.add( light2 );
+  dirLight.shadow.mapSize.width = 2048;
+  dirLight.shadow.mapSize.height = 2048;
 
-  const light2Helper = new THREE.DirectionalLightHelper( light2, 10 );
-  scene.add( light2Helper );
+  dirLight.shadow.camera.far = 45;
+  dirLight.shadow.camera.near = 0.1;
+  dirLight.shadow.camera.fov = 45;
 
+  dirLight.shadow.bias = - 0.0001;
 
-  // GROUND
-  // const groundGeo = new THREE.PlaneGeometry( 10000, 10000 );
-  // const groundMat = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
-  // groundMat.color.setHSL( 0.095, 1, 0.75 );
+  dirLight.castShadow = true;
 
-  // const ground = new THREE.Mesh( groundGeo, groundMat );
-  // ground.position.y = - 0;
-  // ground.rotation.x = - Math.PI / 2;
-  // ground.receiveShadow = true;
-  // scene.add( ground );
+  scene.add( dirLight );
 
-  // const helper = new THREE.CameraHelper( dirLight.shadow.camera );
-  // scene.add( helper );
+  const dirLightHelper = new THREE.DirectionalLightHelper( dirLight, 10 );
+  scene.add( dirLightHelper );
 
-  //debug object
-  const sphereGeometry = new THREE.SphereGeometry( 1, 32, 32 );
-  const sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
-  const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
-  sphere.castShadow = true; //default is false
-  sphere.receiveShadow = false; //default
+  // const pointLight = new THREE.PointLight( 0xffffff, 1, 100 );
+  // pointLight.position.set( 10, 10, 10 );
+  // scene.add( pointLight );
+  
+  // custumised direction light 1
+  // const light1 = new THREE.DirectionalLight( 0xefefff, 0.5 );
+  // light1.position.set( 1, 1, 1 ).normalize();
+  // scene.add( light1 );
+
+  // light1.castShadow = true;
+  // light1.shadowMapBias = -0.001;
+  // light1.shadowMapHeight = 128;
+  // light1.shadowMapWidth = 128;
+  // light1.shadowCameraFar = 500;
+  // light1.shadowCameraNear = 0.1;
+  // light1.shadow.camera.left = 500;
+  // light1.shadow.camera.right = -500;
+  // light1.shadow.camera.top = 500;
+  // light1.shadow.camera.bottom = -50;
+
+  // const light1Helper = new THREE.DirectionalLightHelper( light1, 30 );
+  // scene.add( light1Helper );
+
+  /**
+   * debug object
+  **/  
+  // const sphereGeometry = new THREE.SphereGeometry( 1, 32, 32 );
+  // const sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
+  // const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+  // sphere.castShadow = true; //default is false
+  // sphere.receiveShadow = false; //default
   // scene.add( sphere );
 
   // model loader 
   const loader = new THREE.GLTFLoader();
-  // loader.load( './models/home.gltf', function ( gltf ) {
-  //   console.log(gltf);
-  //   const mesh = gltf.scene.children[ 0 ];
-    
-  //   const S = 1;
-  //   mesh.scale.set( S, S, S ); // scale here
-  //   // mesh.rotateX( -Math.PI * 0.5 );
-  //   mesh.castShadow = true;
-  //   mesh.receiveShadow = true;
-
-  //   scene.add( mesh );
-  // }, undefined, function ( error ) {
-  //   console.error( error );
-  // } );
-
   loader.load( './models/home.gltf', function ( gltf ) {
     console.log(gltf);
     const root = gltf.scene;
 
     root.traverse( function( node ) {
       if ( node.isMesh ) {
-          if ( node.name !== "Cube" ) {
+          if ( node.name === "Cube" || node.name === "whole_body") {
+            console.log("set cube");   
+            // node.material =  new THREE.MeshNormalMaterial();
+            node.receiveShadow = true;
+            node.castShadow = false;
+          } else{            
             node.receiveShadow = true;
             node.castShadow = true;
-          } else{
-            console.log("set cube");
-            node.receiveShadow = true;
           }
         }
     });
@@ -130,27 +141,6 @@ function init(){
   }, function ( error ) {
     console.error( error );
   } );
-
-
-  // const objLoader = new THREE.OBJLoader();
-  // objLoader.load( './models/johnny.obj', function ( object ) {
-  //     // const mesh = object.scene;
-  //     // console.log(object.scene);
-  //     const S = 0.003;
-  //     object.scale.set( S, S, S ); // scale here
-  //     object.position.set( 3, 1.5, 3 );
-  //     object.castShadow = true;
-  //     object.receiveShadow = true;
-  //     scene.add( object );
-  //   }, 	// called when loading is in progresses
-  //   function ( xhr ) {
-  //     console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-  //   },
-  //   // called when loading has errors
-  //   function ( error ) {
-  //     console.log( 'An error happened' );
-  //   }
-  // );
   
   // Axes Helper
   const axesHelper = new THREE.AxesHelper( 5 );
