4c9b6e254b037997f86473ebd05e5f1dde74d082
wallacewangchao <wallacewangchao@gmail.com>
2021-12-26 13:47:43 +0100

    added honda e and robot

--- js/main.js
+++ js/main.js
@@ -1,60 +1,82 @@
 //set up three js
-let tesla;
 let camera, scene, renderer, controls;
+let dirLight, spotLight;
+
+
 const THREEJS_CONTAINER = document.getElementById('threejs-container');
 
 init();
 animate();
 
-function init(){
+function init() {
+
+  initScene();
+
+  // Init gui
+  // const gui = new dat.GUI();
+
+  // const config = {
+  //   spotlightRadius: 4,
+  //   spotlightSamples: 8,
+  //   dirlightRadius: 4,
+  //   dirlightSamples: 8
+  // };
+
+  // const dirlightFolder = gui.addFolder( 'Directional Light' );
+  // dirlightFolder.add( config, 'dirlightRadius' ).name( 'radius' ).min( 0 ).max( 800 ).onChange( function ( value ) {
+
+  //   dirLight.shadow.radius = value;
+  //   console.log(value);
+  // } );
+
+  // dirlightFolder.add( config, 'dirlightSamples', 1, 4000, 1 ).name( 'samples' ).onChange( function ( value ) {
+
+  //   dirLight.shadow.blurSamples = value;
+
+  // } );
+  // dirlightFolder.open();
+
+  window.addEventListener( 'resize', onWindowResize );
+
+}
+
+
+
+function initScene(){
+
   scene = new THREE.Scene();
   scene.background = new THREE.Color( "rgb(255, 255, 255)" );
-  
+  // scene.fog = new THREE.Fog( "rgb(255, 255, 255)", 10, 50 );
+
   camera = new THREE.PerspectiveCamera( 25, THREEJS_CONTAINER.clientWidth / THREEJS_CONTAINER.clientHeight, 0.1, 1000 );
   // camera = new THREE.OrthographicCamera( THREEJS_CONTAINER.clientWidth / - 2, THREEJS_CONTAINER.clientWidth / 2, THREEJS_CONTAINER.clientHeight / 2, THREEJS_CONTAINER.clientHeight / - 2, 1, 1000 );
 
-  renderer = new THREE.WebGLRenderer();
+  renderer = new THREE.WebGLRenderer({ antialias: false });
   renderer.setSize( THREEJS_CONTAINER.clientWidth, THREEJS_CONTAINER.clientHeight );
   
   renderer.outputEncoding = THREE.sRGBEncoding;
   renderer.shadowMap.enabled = true;
   renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
-  // renderer.shadowCameraNear = 1;
-  // renderer.shadowCameraFar = 500;
-  // renderer.shadowCameraFov = 60;
-
-  // renderer.shadowMapBias = -0.0005;
-  // renderer.shadowMapDarkness = 1;
-  // renderer.shadowMapWidth = 2048;
-  // renderer.shadowMapHeight = 2048;
 
   THREEJS_CONTAINER.appendChild( renderer.domElement );
-  
-  
-  // lighting
-  // const ambientLight = new THREE.AmbientLight( "rgb(255, 255, 255)", 1 ); // soft white light
-  // ambientLight.castShadow = true; // default false
-  // scene.add( ambientLight );
-  
-  // const directionalLight = new THREE.DirectionalLight( "rgb(255, 255, 255)", 0.8 );
-  // scene.add( directionalLight );
   
   /**
    * LIGHTS
   **/
-  const hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.5 );
+  const hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.2 );
   hemiLight.color.setHSL( 0.6, 1, 0.6 );
   hemiLight.position.set( 300, 500, 200 );
-  // hemiLight.castShadow = true;
-
   scene.add( hemiLight );
 
-  const hemiLightHelper = new THREE.HemisphereLightHelper( hemiLight, 10 );
-  scene.add( hemiLightHelper );
+  // const hemiLightHelper = new THREE.HemisphereLightHelper( hemiLight, 10 );
+  // scene.add( hemiLightHelper );
+
+  // const light = new THREE.AmbientLight( "rgb(255, 255, 255)", 0.1 ); // soft white light
+  // scene.add( light );
 
   // direction light
-  const dirLight = new THREE.DirectionalLight( "rgb(255, 255, 255)", 1 );
-  dirLight.color.setHSL( 0.1, 1, 0.95 );
+  dirLight = new THREE.DirectionalLight( "rgb(255, 255, 255)", 1 );
+  // dirLight.color.setHSL( 0.1, 1, 0.95 );
   dirLight.position.set( - 1, 1.75, 1 );
   dirLight.position.multiplyScalar( 10 );
 
@@ -73,52 +95,25 @@ function init(){
   dirLight.shadow.camera.fov = 45;
 
   dirLight.shadow.bias = - 0.0001;
-
   dirLight.castShadow = true;
-
   scene.add( dirLight );
 
-  const dirLightHelper = new THREE.DirectionalLightHelper( dirLight, 10 );
-  scene.add( dirLightHelper );
-
-  // const pointLight = new THREE.PointLight( 0xffffff, 1, 100 );
-  // pointLight.position.set( 10, 10, 10 );
-  // scene.add( pointLight );
+  // const dirLightHelper = new THREE.DirectionalLightHelper( dirLight, 6 );
+  // scene.add( dirLightHelper );
   
-  // custumised direction light 1
-  // const light1 = new THREE.DirectionalLight( 0xefefff, 0.5 );
-  // light1.position.set( 1, 1, 1 ).normalize();
-  // scene.add( light1 );
-
-  // light1.castShadow = true;
-  // light1.shadowMapBias = -0.001;
-  // light1.shadowMapHeight = 128;
-  // light1.shadowMapWidth = 128;
-  // light1.shadowCameraFar = 500;
-  // light1.shadowCameraNear = 0.1;
-  // light1.shadow.camera.left = 500;
-  // light1.shadow.camera.right = -500;
-  // light1.shadow.camera.top = 500;
-  // light1.shadow.camera.bottom = -50;
-
-  // const light1Helper = new THREE.DirectionalLightHelper( light1, 30 );
-  // scene.add( light1Helper );
-
-  /**
-   * debug object
-  **/  
-  // const sphereGeometry = new THREE.SphereGeometry( 1, 32, 32 );
-  // const sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xff0000 } );
-  // const sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
-  // sphere.castShadow = true; //default is false
-  // sphere.receiveShadow = false; //default
-  // scene.add( sphere );
+  const dirLight2 = new THREE.DirectionalLight( "rgb(255, 255, 255)", 0.5 );
+  dirLight2.position.set( 1, 1.75, 0 );
+  // dirLight2.position.multiplyScalar( 5 );
+  dirLight2.castShadow = false;
+  scene.add( dirLight2 );
 
   // model loader 
   const loader = new THREE.GLTFLoader();
-  loader.load( './models/home.gltf', function ( gltf ) {
+  loader.load( './models/scene.gltf', function ( gltf ) {
     console.log(gltf);
     const root = gltf.scene;
+    root.castShadow = true;
+    gltf.scene.receiveShadow = true;
 
     root.traverse( function( node ) {
       if ( node.isMesh ) {
@@ -137,28 +132,37 @@ function init(){
     console.log(gltf);
 
   }, function(xhr){
-    console.log(xhr.loaded/xhr.total * 100) + "% loaded";
+    // console.log(xhr.loaded/xhr.total * 100) + "% loaded";
   }, function ( error ) {
     console.error( error );
   } );
   
+
+  const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 100, 100 ), new THREE.MeshPhongMaterial( { color: "rgb(255, 240, 210)", depthWrite: false } ) );
+  mesh.rotation.x = - Math.PI / 2;
+  mesh.receiveShadow = true;
+  scene.add( mesh );
+  console.log( mesh );
+
   // Axes Helper
-  const axesHelper = new THREE.AxesHelper( 5 );
-  scene.add( axesHelper );
+  // const axesHelper = new THREE.AxesHelper( 5 );
+  // scene.add( axesHelper );
   
   // set camera positions
   camera.position.set( 10, 10, 10 ); // Set position like this
   // camera.lookAt(new THREE.Vector3(0,0,0)); // Set look at coordinate like this
-  
 
   // orbit control
   controls = new THREE.OrbitControls( camera, renderer.domElement );
   controls.update();
   controls.addEventListener( 'change', render );
   controls.minPolarAngle = Math.PI * 0.25;
-  controls.maxPolarAngle = Math.PI * 0.25;
+  // controls.maxPolarAngle = Math.PI * 0.25;
   controls.target.set( 0, 0.5, 0 );
   controls.enableDamping = true;
+
+  window.addEventListener( 'resize', onWindowResize );
+
 }
 
 
@@ -178,6 +182,12 @@ function animate() {
   controls.update(); // required if damping enabled
   render();
 
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
 /**
